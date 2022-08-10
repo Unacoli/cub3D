8e655c0df7bcc1479ecdb94e41596a0963cef380
telli <telli1@hotmail.fr>
2022-08-10 17:38:59 +0200

    Start of project, compilation error quick fix to do

---
+++ srcs/game_utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 16:34:15 by tmoragli          #+#    #+#             */
+/*   Updated: 2022/08/10 17:09:53 by tmoragli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	allocate_game_data(t_data *data, char **av)
+{
+	if (!ft_malloc((void **)&(data->mlx), sizeof(t_window)))
+		return (1);
+	if (!ft_malloc((void **)&(data->m_info), sizeof(t_map)))
+		return (1);
+	data->map_path = ft_strdup(av[1]);
+	if (!data->map_path)
+		return (1);
+	return (0);
+}
+
+void	init_subdata(t_data *data)
+{
+	ft_memset(data->mlx, 0, sizeof(t_window));
+	ft_memset(data->m_info, 0, sizeof(t_map));
+}
+
+void	init_game_data(t_data *data, char **av)
+{
+	ft_memset(data, 0, sizeof(t_data));
+	if (allocate_game_data(data, av))
+		exit_game(data, "Failed allocation, exiting\n");
+	init_subdata(data);
+}
+
+void	free_game_data(t_data *data)
+{
+	free(data->mlx);
+	free(data->m_info);
+	free(data);
+}
+
+void	exit_game(t_data *data, char *msg)
+{
+	ft_printf("%s\n", msg);
+	free_game_data(data);
+	exit(1);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,41 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:49:09 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/08/10 02:07:50 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:07:45 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	game_loop(t_data *data)
+{
+	(void)data;
+	while (1)
+	{
+		printf("game started\n");
+		return ;
+	}
+}
+
+int	cube_3d(t_data *data)
+{
+	if (parsing(data))
+		return (ft_error(data->err_msg));
+	else
+		game_loop(data);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
-	printf("Hello world\n");
+	t_data	*data;
+
+	if (ac > 2)
+		return (ft_error("Too many arguments, try ./cub3D \"map_path.cub\"\n"));
+	if (!ft_malloc((void **)&(data), sizeof(t_data)))
+		return (1);
+	init_game_data(data, av);
+	cube_3d(data);
+	free_game_data(data);
 	return (0);
 }
